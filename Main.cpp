32e6bcd334d0e541d5412c79fabe1c11a17958b5
Pablo Marquez <124933671+PabloMarquezCapilla@users.noreply.github.com>
2024-11-01 11:01:53 -0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,46 @@
+
+#include "persona.h"
+#include <iostream>
+ #include <string>
+using namespace std;
+int main() {
+
+    Persona P;
+    Autor A;
+    string nombre;
+    double Dni;
+    string medio;
+    Noticia noticia;
+    int opcion;
+
+        cout << "Bienvenidos a Daily Bugle" << endl;
+        cout << "1. Publicar una noticia" << endl;
+        cout << "2. Leer noticias" << endl;
+        cout << "Ingrese su opcion: ";
+        cin >> opcion;
+
+     
+    if (opcion == 1)
+    {
+        cout<<"Indique su Nombre:";
+                cin >> nombre;
+                A.setnombre(nombre);
+                cout<<"Indique su DNI";
+                cin >> Dni;
+                A.setdni(Dni);
+                 cout<<"Indique su Medio";
+                cin >> medio;
+                A.setmedio(medio);
+            
+                cout<<"Usted se Registro Exitosamente"<<endl;
+                A.mostrarautor();
+    }
+    
+                
+               
+                
+       
+       
+
+
+}
